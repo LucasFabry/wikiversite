bf7a77467e94925929309d0c8c16fbcda2c57506
LucasFabry <77359139+LucasFabry@users.noreply.github.com>
2022-01-05 11:34:20 +0100

    Premier commit

---
+++ Cours.java
@@ -0,0 +1,20 @@
+
+public class Cours {
+	private String nomCours;
+	
+	
+	public Cours(String nomCours) {
+		this.nomCours = nomCours;
+	}
+
+
+	public String getNomCours() {
+		return nomCours;
+	}
+
+	
+	public void setNomCours(String nomCours) {
+		this.nomCours = nomCours;
+	}	
+	
+}
