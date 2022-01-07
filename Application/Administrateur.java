5341560832bc955eea455187d8f32aea9ffb1970
Silverakai <81218138+Silverakai@users.noreply.github.com>
2022-01-07 10:39:48 +0100

    bannir+certif+listes

--- Application/Administrateur.java
+++ Application/Administrateur.java
@@ -3,31 +3,43 @@ package Application;
 import interfaceGraphique.FenetreMembre;
 
 public class Administrateur extends Membre {
+	private ArrayList listeMembres = new ArrayList<Membre>();
+	private ArrayList listeMembresBannis = new ArrayList<Membre>();
+	private ArrayList listeMembreCertifie = new ArrayList<MembreCertifie>();
 
 	public Administrateur(String pseudo, String password) {
 		super(pseudo, password);
 		// TODO Auto-generated constructor stub
 	}
 	
+	//RÃ©cupÃ©rer la liste des membres
+	public ArrayList getMembres(){
+		return this.listeMembres;
+	}
+	
 	/**
 	 * 
-	 * Méthode pour bannir un membre
-	 * Idée : penser à créer un concierge (comme l'an dernier) qui gère la liste des membres
-	 * cette méthode retire simplement un membre de la liste
+	 * MÃ©thode pour bannir un membre
+	 * IdÃ©e : penser Ã  crÃ©er un concierge (comme l'an dernier) qui gÃ¨re la liste des membres
+	 * cette mÃ©thode retire simplement un membre de la liste
 	 * 
 	 * */
 	public void bannirMembre(Membre m){
-		//TODO
+		this.listeMembresBannis.add(m);
+		this.listeMembres.remove(m);	
 	}
 	/**
 	 * 
-	 * Méthode pour bannir un membre
-	 * Idée : penser à créer un concierge (comme l'an dernier) qui gère la liste des membres
-	 * cette méthode permet de certifier un membre
+	 * MÃ©thode pour bannir un membre
+	 * IdÃ©e : penser Ã  crÃ©er un concierge (comme l'an dernier) qui gÃ¨re la liste des membres
+	 * cette mÃ©thode permet de certifier un membre
 	 * 
 	 * */
 	public void certifierMembre(Membre m){
 		//TODO
+		MembreCertifie mc = new MembreCertifie(m.getPseudo(), m.getPassword());
+		this.listeMembreCertifie.add(m);
+		m=null;
 	}
 	
 	public void bannirIP(String ipAdress){
