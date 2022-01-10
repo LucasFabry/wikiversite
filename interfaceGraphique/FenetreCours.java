50fb0b97dce99fbb83288bc49c5f34e2ea2e15d4
LucasFabry <77359139+LucasFabry@users.noreply.github.com>
2022-01-10 16:26:04 +0100

    Add files via upload

---
+++ interfaceGraphique/FenetreCours.java
@@ -0,0 +1,7 @@
+package interfaceGraphique;
+
+import javax.swing.JFrame;
+
+public class FenetreCours extends JFrame {
+	
+}
--- interfaceGraphique/FenetreMembreConnecte.java
+++ interfaceGraphique/FenetreMembreConnecte.java
@@ -5,12 +5,18 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
 import Application.Membre;
 
 public class FenetreMembreConnecte extends JFrame implements ActionListener {
 
+	private DefaultListModel<String> listeCours = new DefaultListModel<>();
+	private JButton boutonOuvrirCo = new JButton("Ouvrir un cours");
 	
 	public FenetreMembreConnecte(Membre m){
 		// TODO Auto-generated constructor stub
@@ -18,14 +24,36 @@ public class FenetreMembreConnecte extends JFrame implements ActionListener {
 	    this.setSize(700, 500);
 	    this.setLocationRelativeTo(null);  
 	    this.setLayout(new BorderLayout());
-	    
 	    this.setVisible(true);
+	    
+	    JPanel zoneEst = new JPanel();
+	    zoneEst.add(new JList(listeCours));
+	    
+	    /*
+	     * 
+	     * Liste des cours disponibles, un simple membre ne peut que consulter la liste des cours et en ouvrir un.
+	     * 
+	     * **/
+	    listeCours.add(0, "coursTropBien");
+	    
+	    
+	    JPanel zoneSud = new JPanel();
+	    zoneSud.add(boutonOuvrirCo);
+	    
+	    this.getContentPane().add(zoneSud, BorderLayout.SOUTH);
+	    this.getContentPane().add(zoneEst, BorderLayout.EAST);
+	    
+	    boutonOuvrirCo.addActionListener(this);
 	    
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+		if(e.getSource() == "boutonOuvrirCo"){
+			
+		}
 
 	}
 
